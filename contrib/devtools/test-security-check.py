c1e2c437f3b2ce0c1095dcafb020984c9b67a76b
isle2983 <isle2983@yahoo.com>
2017-01-18 18:53:48 -0700

    Remove semicolons that end lines in Python

--- contrib/devtools/test-security-check.py
+++ contrib/devtools/test-security-check.py
@@ -16,8 +16,8 @@ def write_testcode(filename):
     #include <stdio.h>
     int main()
     {
-        printf("the quick brown fox jumps over the lazy god\\n");
-        return 0;
+        printf("the quick brown fox jumps over the lazy god\\n")
+        return 0
     }
     ''')
 
